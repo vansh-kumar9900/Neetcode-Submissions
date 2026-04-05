2a47b90514dc79740bc0e18921f6192aa1a5a225
vansh-kumar9900 <156178820+vansh-kumar9900@users.noreply.github.com>
2026-04-05 23:53:47 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] productExceptSelf(int[] arr) {
+        int n = arr.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        prefix[0] = 1;
+        suffix[n-1] = 1;
+
+        for(int i = 1; i < n; i++){
+            prefix[i] = arr[i-1]*prefix[i-1];
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            suffix[i] = suffix[i+1]*arr[i+1];
+        }
+
+        for(int i = 0; i < n; i++){
+            arr[i] = prefix[i]*suffix[i];
+        }
+
+        return arr;
+    }
+}  
